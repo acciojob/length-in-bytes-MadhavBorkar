8244615a5960c3baf997b4b578c416f5be65c981
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-04-02 19:07:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const byteSize = (str) => {
   // write your code here
-};
-function byteSize(str) {
-    const blob = new Blob([str]);
-    const size = blob.size;
+	function byteSize(str) {
+    const Byte = new Byte([str]);
+    const size = Byte.size;
 
     return size;
 }
+};
+
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
